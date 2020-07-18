eca546a2c8fb00eaa7de46e37401bc49b382a9c8
Unknown <hc.ngu01@gmail.com>
2020-07-18 22:38:10 +0800

    removed antd

--- client/src/containers/home/LoginForm.jsx
+++ client/src/containers/home/LoginForm.jsx
@@ -1,28 +1,12 @@
 import React  from 'react';
 import {useHistory} from 'react-router-dom'
-import "antd/dist/antd.css";
-import { Form, Input, Button, Checkbox, Row, Col} from "antd";
 
 
-const layout = {
-    labelCol: {
-        span: 24,
-    },
-    wrapperCol: {
-        span: 24,
-    },
-};
-const tailLayout = {
-    wrapperCol: {
-        offset: 8,
-        span: 16,
-    },
-};
 function LoginForm(){
 
     const history = useHistory()
 
-  const onFinish = async (values) => {
+  const onSubmit = async (data) => {
       console.log("Success:", values);
       // call api to login
       console.log({values});
@@ -49,59 +33,14 @@ function LoginForm(){
         // localhost/api/posts
       
 
-
-
   };
 
-  const onFinishFailed = (errorInfo) => {
-      console.log("Failed:", errorInfo);
-  };
 
  return (
-     <Form
-         name="basic"
-         initialValues={{
-             remember: true,
-         }}
-         onFinish={onFinish}
-         onFinishFailed={onFinishFailed}
-     >
-         <Form.Item
-             label="Email"
-             name="username"
-             rules={[
-                 {
-                     required: true,
-                     message: "Please enter your email!",
-                 },
-             ]}
-         >
-             <Input  />
-         </Form.Item>
-
-         <Form.Item
-             label="Password"
-             name="password"
-             rules={[
-                 {
-                     required: true,
-                     message: "Please input your password!",
-                 },
-             ]}
-         >
-             <Input.Password />
-         </Form.Item>
-
-         <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-             <Checkbox>Remember me</Checkbox>
-         </Form.Item>
-
-         <Form.Item {...tailLayout}>
-             <Button type="primary" htmlType="submit">
-                 Submit
-             </Button>
-         </Form.Item>
-     </Form>
+     <section>
+         
+     </section>
+     
  );
 
 }
