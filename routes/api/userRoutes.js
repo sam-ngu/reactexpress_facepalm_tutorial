138c53673f56888ec7d26280165b3509549ca8ac
Unknown <hc.ngu01@gmail.com>
2020-07-18 11:19:37 +0800

    user api routes done, not tested

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const User = require('./../../models/User');
+const router = express.Router();
+
+router.get("users", (req, res) => {
+    
+    User.find({}).then((results) => {
+        res.json({
+            data: results,
+        })
+    })
+
+
+});
+router.get('users/:id', (req, res) => {
+    User.findById(req.params.id).then((result) => {
+        res.json({
+            data: result
+        })
+    })    
+})
+
+router.post('users', (req, res) => {
+
+    // validation 
+
+
+    User.create(req.body).then((created)=> {
+        res.json({
+            data: created
+        })
+    })
+
+
+})
+
+router.patch('users/:id', (req, res) => {
+
+
+    User.findByIdAndUpdate(req.params.id, 
+        {
+            $push: {
+                email: req.body.email,
+                name: req.body.name,
+                password: req.body.password,
+            },
+        },
+        { new: true, runValidators: true }
+    ).then((updated) => {
+        res.json({
+            data: updated,
+        });
+    });
+    
+
+})
+
+router.delete('users/:id', (req, res) => {
+    User.findByIdAndDelete(req.params.id).then((deleted) => {
+        res.json({
+            data: true
+        })
+    })
+
+
+})
+
+
+module.exports = router;
