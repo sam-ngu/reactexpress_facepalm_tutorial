5c11f149f0394280799a1b554e502ecae585e006
Luke Nagy <ljsn@protonmail.com>
2020-07-18 15:58:07 +0800

    actually getting passport to work

--- client/src/containers/home/LoginForm.jsx
+++ client/src/containers/home/LoginForm.jsx
@@ -26,6 +26,9 @@ function LoginForm(){
         const response = await fetch('http://localhost:3001/api/login', {
             mode: 'cors',
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({
                 email: values.username,
                 password: values.password,
--- routes/api/auth/auth.js
+++ routes/api/auth/auth.js
@@ -1,32 +1,10 @@
 const express = require('express');
 const passport = require('./../../../config/passport');
-const { Strategy: LocalStrategy } = require("passport-local");
 
 const router = express.Router();
 
-const localStrategy = new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-  console.log({email});
-  User.findOne({ email: email.toLowerCase() }, (err, user) => {
-    if (err) { return done(err); }
-    if (!user) {
-        // done(err, user, info pass back to controller)
-      return done(null, false, { msg: `Email ${email} not found.` });
-    }
-    if (!user.password) {
-      return done(null, false, { msg: 'Please enter a password.' });
-    }
-    user.comparePassword(password, (err, isMatch) => {
-      if (err) { return done(err); }
-      if (isMatch) {
-        return done(null, user);
-      }
-      return done(null, false, { msg: 'Invalid email or password.' });
-    });
-  });
-});
-
 router.post('/login', (req, res, next) => {
-    passport.authenticate(localStrategy, (err, user, info) => {
+    passport.authenticate('local', (err, user, info) => {
         if (err) {
             return next(err);
         }
@@ -36,13 +14,8 @@ router.post('/login', (req, res, next) => {
                 data: info.message
             });
         }
-        req.logIn(user, (err) => {
-            if (err) {
-                return next(err);
-            }
-            req.json({
-                data: user
-            })
+        res.json({
+            data: user
         });
     })(req, res, next);
 });
